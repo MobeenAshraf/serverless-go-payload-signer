8fbf421d0200582955fe2e70fa92e9d4b5a95e84
Mobeen <mobeen.dude786@gmail.com>
2021-06-27 01:39:08 +0500

    Initial Commit

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Print("Request body: ", request.Body)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(request.Body), &result)
+	log.Print("Result body: ", result)
+	log.Print("Result val: ", result["mobeen"])
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello AWS Lambda and Netlify",
